0071ce24d816f950b82ec88839b00248b351cdbc
Daniel Josh <danieludumajosh@gmail.com>
2025-07-11 12:10:51 +0100

    fix: changed node version for git actions

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
+    output: 'export',
 };
 
 const isProd = process.env.NODE_ENV === 'production';
