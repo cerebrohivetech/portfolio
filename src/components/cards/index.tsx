75c7c76af44166c2eea39d8c9b38fc3de806fff7
Daniel Josh <danieludumajosh@gmail.com>
2025-09-03 11:48:21 +0100

    fix: landing-page Hero section

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -1,6 +1,5 @@
 import Brands, { Companies } from "./brands";
 import Product, { CaseStudyDisplay } from "./product";
 import { ServiceCard } from "./services";
-import { motion } from "framer-motion";
 
-export { Brands, Companies, Product, CaseStudyDisplay, ServiceCard, motion };
+export { Brands, Companies, Product, CaseStudyDisplay, ServiceCard };
